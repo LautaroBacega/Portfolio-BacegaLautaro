3d04aadc0d4c9f9d45baa903ffb005c17f538b19
LautaroBacega <lautibacega@gmail.com>
2025-01-03 19:29:16 -0300

    actualizacion cv

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './about.css'
 import AboutImg from '../../assets/FotoCv.jpg'
-import CV from '../../assets/John-Cv.pdf'
+import CV from '../../assets/Lautaro_Bacega_CV 2.pdf'
 import Info from './Info'
 
 const About = () => {
