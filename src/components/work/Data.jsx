b9e51fd0074af2dfc00321efd9207dc202350fa9
LautaroBacega <lautibacega@gmail.com>
2025-07-17 13:46:09 -0300

    Add CineLista project to projectsData
    Added CineLista project details and image to the projectsData array in Data.jsx. Also added the CineLista.png asset to the assets directory.

--- src/components/work/Data.jsx
+++ src/components/work/Data.jsx
@@ -2,6 +2,7 @@ import EcommerceIMG from '../../assets/EcommerceIMG.png';
 import CoderBackendIMG from '../../assets/CoderBackendImg.jpeg';
 import abbmxFreestyleIMG from '../../assets/abbmxFreestyleIMG.png';
 import autenticacionIMG from '../../assets/autenticacionIMG.png'
+import CineListaIMG from '../../assets/CineLista.png'
 
 export const projectsData = [
     {
@@ -13,24 +14,31 @@ export const projectsData = [
     },
     {
         id: 2,
-        image: CoderBackendIMG,
-        title: 'Proyecto Final Desarrollo Backend',
-        category: 'Backend',
-        link: 'https://github.com/LautaroBacega/Entrega-Final---Bacega-Lautaro---Backend-3.git',
+        image:  CineListaIMG,
+        title: 'CineLista',
+        category: 'Fullstack',
+        link: 'https://movieapp-8x3t.onrender.com/',
     },
     {
         id: 3,
+        image: autenticacionIMG,
+        title: 'Autenticación',
+        category: 'Fullstack',
+        link: 'https://autenticacion-o8nx.onrender.com/',
+    },
+    {
+        id: 4,
         image: abbmxFreestyleIMG,
         title: 'Escuela de BMX Freestyle',
         category: 'Frontend',
         link: 'https://abbmxfreestyle.netlify.app/',
     },
     {
-        id: 4,
-        image: autenticacionIMG,
-        title: 'Autenticación',
-        category: 'Fullstack',
-        link: 'https://autenticacion-o8nx.onrender.com/',
+        id: 5,
+        image: CoderBackendIMG,
+        title: 'Proyecto Final Desarrollo Backend',
+        category: 'Backend',
+        link: 'https://github.com/LautaroBacega/Entrega-Final---Bacega-Lautaro---Backend-3.git',
     }
 ]
 
